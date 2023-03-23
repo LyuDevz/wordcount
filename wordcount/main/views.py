6af0dfebb27a7cdaba28c44f841f6dc44eee1179
LyuDevz <lyuii0726@gmail.com>
2023-03-23 23:53:40 +0900

    first commit

---
+++ wordcount/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def main(request):
+    return render(request, 'main.html')
+
+def result(request):
+    text=request.GET["text"]
+    text_list=text.split()
+    text_dict={}
+    for word in text_list :
+        if word in text_dict :
+            text_dict[word] +=1
+        else:
+            text_dict[word] = 1
+    return render(request, 'result.html', {'words':text_dict.items()})
